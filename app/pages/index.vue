2b0afbba2af6341282f9eecdcc5aeea78699f1d0
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2025-09-19 16:29:56 +0700

    chore: update dependencies and add ui-thing configuration
    - Added new dependencies for Nuxt, Tailwind CSS, and UI components.
    - Introduced devDependencies for code formatting and TypeScript support.
    - Created a new configuration file for ui-thing with specified paths and settings.

---
+++ app/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <UiContainer class="relative flex flex-col items-center py-10 text-center lg:py-20">
+    <div>
+      <UiBadge variant="outline" class="px-3 py-1.5 text-sm font-normal lg:py-2">
+        <Icon name="lucide:shield-check" class="mr-1 inline size-4" />
+        Aman, Data Tetap di Perangkat Anda</UiBadge
+      >
+    </div>
+
+    <h1 class="mt-7 mb-4 text-4xl font-bold lg:mt-5 lg:mb-6 lg:text-center lg:text-5xl xl:text-6xl">
+      Buat Invoice Instan, Tanpa Ribet Login
+    </h1>
+    <p class="text-muted-foreground mx-auto max-w-[760px] text-lg lg:text-center lg:text-xl">
+      Invoisy membantu freelancer dan profesional membuat invoice profesional dalam hitungan menit.
+      Tidak perlu akun, cukup isi detailnya dan dapatkan invoice siap kirim.
+    </p>
+
+    <div class="mt-8 grid w-full grid-cols-1 items-center gap-3 sm:flex sm:justify-center lg:mt-10">
+      <UiButton size="lg">
+        Buat Invoice Sekarang
+        <Icon name="lucide:plus-circle" />
+      </UiButton>
+    </div>
+  </UiContainer>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,47 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+
+  modules: ["@nuxtjs/color-mode", "motion-v/nuxt", "@vueuse/nuxt", "@nuxt/icon", "@nuxt/fonts"],
+
+  imports: {
+    imports: [
+      {
+        from: "tailwind-variants",
+        name: "tv",
+      },
+      {
+        from: "tailwind-variants",
+        name: "VariantProps",
+        type: true,
+      },
+    ],
+  },
+
+  colorMode: {
+    preference: "light",
+    storageKey: "invoisy-color-mode",
+    classSuffix: "",
+  },
+
+  icon: {
+    clientBundle: {
+      scan: true,
+      sizeLimitKb: 0,
+    },
+
+    mode: "svg",
+    class: "shrink-0",
+    fetchTimeout: 2000,
+    serverBundle: "local",
+  },
+
+  css: ["~/assets/css/tailwind.css"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
