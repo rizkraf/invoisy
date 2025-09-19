414df7a01123967e0f37c447bb1c13ddee2b1a38
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2025-09-19 17:41:06 +0700

    feat: add header and footer components with layout integration

---
+++ app/components/Site/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <UiContainer
+    as="footer"
+    class="flex flex-col items-center justify-between gap-7 py-12 lg:flex-row"
+  >
+    <ul class="flex flex-wrap gap-5">
+      <li v-for="k in links" :key="k.name">
+        <NuxtLink class="hover:text-primary" :to="k.href">{{ k.name }}</NuxtLink>
+      </li>
+    </ul>
+
+    <p class="text-muted-foreground">&copy; {{ new Date().getFullYear() }} {{ websiteName }}</p>
+  </UiContainer>
+</template>
+
+<script setup lang="ts">
+  const websiteName = "Invoisy";
+  const links = [
+    { name: "Beranda", href: "/" },
+    { name: "Privasi", href: "/privacy" },
+    { name: "Ketentuan", href: "/terms" },
+  ];
+</script>
---
+++ app/components/Site/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="bg-background/90 z-20 backdrop-blur">
+    <UiContainer class="flex h-16 items-center justify-between lg:h-20">
+      <div class="flex items-center gap-10">
+        <NuxtLink to="/" class="flex items-center gap-3">
+          <span class="font-semibold lg:text-lg">Invoisy</span>
+        </NuxtLink>
+      </div>
+      <div class="items-center gap-3">
+        <UiButton to="/generate" variant="outline" size="sm"> Buat Invoice </UiButton>
+      </div>
+    </UiContainer>
+  </header>
+</template>
